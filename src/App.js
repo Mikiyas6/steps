1898ac84802a24ef633b6fe2beb83f337cd7322e
Mikiyas6 <Mikitedi9@gmail.com>
2024-09-25 01:58:50 +0300

    I learned about updating the state based on the current state value using callback function.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 
 const messages = [
   "Learn React ⚛️",
@@ -7,6 +7,7 @@ const messages = [
 ];
 export default function App() {
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
   function handlePrevious() {
     step >= 2 && setStep((step) => step - 1);
   }
@@ -14,31 +15,42 @@ export default function App() {
     step <= 2 && setStep((step) => step + 1);
   }
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step === 1 && "active"}>1</div>
-        <div className={step === 2 && "active"}>2</div>
-        <div className={step === 3 && "active"}>3</div>
-      </div>
-      <p className="message">
-        Step {step}: {messages[step - 1]}
-      </p>
-      <div className="buttons">
-        <button
-          onClick={handlePrevious}
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          type="text"
-        >
-          previous
-        </button>
-        <button
-          onClick={handleNext}
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          type="text"
-        >
-          next
-        </button>
-      </div>
-    </div>
+    <Fragment>
+      <button
+        className="close"
+        onClick={() => setIsOpen((isOpen) => !isOpen)}
+        type="text"
+      >
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step === 1 && "active"}>1</div>
+            <div className={step === 2 && "active"}>2</div>
+            <div className={step === 3 && "active"}>3</div>
+          </div>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              onClick={handlePrevious}
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              type="text"
+            >
+              previous
+            </button>
+            <button
+              onClick={handleNext}
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              type="text"
+            >
+              next
+            </button>
+          </div>
+        </div>
+      )}
+    </Fragment>
   );
 }
